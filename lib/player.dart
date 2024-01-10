fe1a63371faf1d294dc5d49bfa4f0c1a842a8b1a
Birame S. <birame.sene@zenika.com>
2024-01-11 00:00:49 +0100

    implemented rule: a game is over when all fields in a column are taken by a player

---
+++ lib/player.dart
@@ -0,0 +1,5 @@
+class Player {
+  final String symbol;
+
+  Player(this.symbol);
+}
--- test/tic_tact_toe_kata_test.dart
+++ test/tic_tact_toe_kata_test.dart
@@ -5,11 +5,15 @@ import 'package:test/test.dart';
 void main() {
   test("a game is over when all fields are taken", () {
     var game = Game(board: FullBoard());
-    expect(game.isOver(), true);
+    expect(game.result(), "It's a draw!");
   });
-  test("a game is NOT over when field 3x1 is free", () {
-    var game = Game(board: NonFullBoard());
-    expect(game.isOver(), false);
+  test("player X wins when taked all fields in the 1st column", () {
+    var game = Game(board: PlayerXCompleted1stColumn());
+    expect(game.result(), "Player X wins!");
+  });
+  test("player O wins when taked all fields in the 2nd column", () {
+    var game = Game(board: PlayerOCompleted2ndColumn());
+    expect(game.result(), "Player O wins!");
   });
   test("a game is NOT over when field 3x3 is free", () {
     var game = Game(board: NonFullBoard());
